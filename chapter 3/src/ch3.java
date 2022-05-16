69b9696d330d0f6e6893a39f4f6b97676928c563
Daniel710s <100789093+Daniel710s@users.noreply.github.com>
2022-05-16 09:07:10 -0600

    9:06AM

--- chapter 3/src/ch3.java
+++ chapter 3/src/ch3.java
@@ -1,9 +1,163 @@
+System.out.println ("Great day for surfing!")
 
-public class ch3 {
+System.out.println ("The waves today are 7ft!")
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+if (<condition>)  {
+	<Staementa>
+}  else  {
+	<Statementa>
 }
+
+if (guess == SECRET_NUM)  {
+	Syatem.out.println("You gueased it!")
+} else {
+	Syatem.out.println("Try again.");
+}
+
+
+System.out.println("Great day for Surfing!")
+
+System.out.println("Waves are 5ft!")
+
+System.out.println("Go body boarding!")
+
+if (guess == SECRET_NUM)  {     //correct
+	Syatem.out.println("You guessed it!");
+} else {
+	if (guess < SECRET_NUM) {   //too low
+		Syatem.out.println("Too low.*);
+	} else {                         //too high
+		Syatem.out.println("Too high.");
+	}
+}
+
+System.out.println("Great day for surfing!")     
+
+System.out.println("Waves are 3 feet today, Go for a swim")
+
+                 
+
+
+System.out.println("Hurricane wind speed")
+                       
+Category 1: 74-95 mph or 64-82 kt or 119-1553km/hr
+
+Category 2: 96-110 mph or 83-95kt or 154-177km/hr
+
+Category 3: 111-130 mph or 96-113 kt or 178-209 km/hr
+
+Category 4: 131-155 mph or 114-135 kt or 210-249 km/hr
+
+Category5: Greater than 155 mph or 135 kt or 249 km/hr
+
+
+public static void main(String) {} args) {
+			Random r = new Random(10);
+		}
+
+System.out.println("RandomNum")
+
+System.out.println("First number")  " + r.nextInt(62);
+		
+System.out.println("Second number")  " + r.nextInt(81);
+	
+System.out.println("Third number")  " + r.nextInt(8);	
+
+System.out.println("Fourth number")  " + r.nextInt(23);
+
+System.out.println("Fifth number")  " + r.nextInt(44);
+	
+	
+{
+	
+}
+	
+	         	
+                    
+system.out.println("RandomNum")                
+                               
+"15-30"
+
+
+"Enter the number of copies to be printed: 750"
+
+"Price per copy is: $0.26"
+
+"Total cost is $195.00"
+
+
+"Enter the percentage: 91"
+
+"The corresponding letter grade is: A"
+
+
+
+"What is 18 + 10 28"
+"Correct!"
+
+
+
+1. "Grade 95 Great job!"
+
+   "Grade 17 Error"
+
+
+
+2. 
+     First number is Larger.
+     Second number is Larger.
+     Numbers are equal.
+
+
+
+
+
+3. 
+if (num + 7 == 10)  {
+	  System.out.println(*___ number*);
+} else {
+		System.out.println(*___ number*);  
+}
+
+
+4. 
+
+a) 25
+b) 50
+c) 15
+
+
+5. 
+   if (age < 18)  {
+	   System.out.println(*child*);
+   }  else if (age > 18 && age < 65)  {
+	   System.out.println(*adult*);
+   }  else if(age > 65)  {
+	   System.out.println(*senior*);
+   }
+
+
+6.
+  size > 50 && weight == 50 "F"
+  
+  value < 100 && !(weight == 50) "T"
+
+  size >= 100 {} value >= 100 "T"
+
+  weight < 50 {} value > 50 "F"
+  
+  !(value < 75) "T"
+  
+  !(size > 100 && weight >50 && value > 75) "F"
+
+ (value < 125 {} weight < 76) && size ==100 "T"
+
+      
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
